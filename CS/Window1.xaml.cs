945b237bd723930f57dadb3fe4732cfd0fb16186
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Window1.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace ConditionalCellFormatting {
+
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+        }
+    }
+
+    public class SimpleData {
+        public string Column1 { get; set; }
+        public int Column2 { get; set; }
+
+        public SimpleData() { }
+
+        public SimpleData(string str1, int int2) {
+            Column1 = str1;
+            Column2 = int2;
+        }
+    }
+
+    public class ColorValueConverter : MarkupExtension, IValueConverter {
+        public int MaxValue { get; set; }
+
+        #region IValueConverter Members
+        public object Convert(object value, Type targetType, object parameter, 
+        CultureInfo culture) {
+            int count = MaxValue - (int)value;
+            Color color = Color.FromRgb(0xff, 0xad, 0x5d);
+
+            int r = color.R + (0xff - color.R) * count / MaxValue;
+            int g = color.G + (0xff - color.G) * count / MaxValue;
+            int b = color.B + (0xff - color.B) * count / MaxValue;
+
+            return new SolidColorBrush(Color.FromArgb(255, (byte)r, (byte)g, (byte)b));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, 
+        CultureInfo culture) {
+            return null;
+        }
+        #endregion
+
+        public override object ProvideValue(IServiceProvider serviceProvider) {
+            return this;
+        }
+    }
+}
---
+++ VB/Window1.xaml.vb
@@ -0,0 +1,81 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Globalization
+Imports System.Windows
+Imports System.Windows.Data
+Imports System.Windows.Markup
+Imports System.Windows.Media
+
+Namespace ConditionalCellFormatting
+
+	Partial Public Class Window1
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+	End Class
+
+	Public Class SimpleData
+		Private privateColumn1 As String
+		Public Property Column1() As String
+			Get
+				Return privateColumn1
+			End Get
+			Set(ByVal value As String)
+				privateColumn1 = value
+			End Set
+		End Property
+		Private privateColumn2 As Integer
+		Public Property Column2() As Integer
+			Get
+				Return privateColumn2
+			End Get
+			Set(ByVal value As Integer)
+				privateColumn2 = value
+			End Set
+		End Property
+
+		Public Sub New()
+		End Sub
+
+		Public Sub New(ByVal str1 As String, ByVal int2 As Integer)
+			Column1 = str1
+			Column2 = int2
+		End Sub
+	End Class
+
+	Public Class ColorValueConverter
+		Inherits MarkupExtension
+		Implements IValueConverter
+		Private privateMaxValue As Integer
+		Public Property MaxValue() As Integer
+			Get
+				Return privateMaxValue
+			End Get
+			Set(ByVal value As Integer)
+				privateMaxValue = value
+			End Set
+		End Property
+
+		#Region "IValueConverter Members"
+		Public Function Convert(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IValueConverter.Convert
+			Dim count As Integer = MaxValue - CInt(Fix(value))
+			Dim color As Color = Color.FromRgb(&Hff, &Had, &H5d)
+
+			Dim r As Integer = color.R + (&Hff - color.R) * count / MaxValue
+			Dim g As Integer = color.G + (&Hff - color.G) * count / MaxValue
+			Dim b As Integer = color.B + (&Hff - color.B) * count / MaxValue
+
+			Return New SolidColorBrush(Color.FromArgb(255, CByte(r), CByte(g), CByte(b)))
+		End Function
+
+		Public Function ConvertBack(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IValueConverter.ConvertBack
+			Return Nothing
+		End Function
+		#End Region
+
+		Public Overrides Function ProvideValue(ByVal serviceProvider As IServiceProvider) As Object
+			Return Me
+		End Function
+	End Class
+End Namespace
